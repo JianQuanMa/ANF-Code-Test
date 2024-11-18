fd0d0d30f5a8011981084173ae63ab2c3cad064b
Jian Ma <jianquanma@gmail.com>
2024-11-17 19:12:00 -0500

    minor code fixes

--- ANF Code Test/ExploreContentCell.swift
+++ ANF Code Test/ExploreContentCell.swift
@@ -25,41 +25,67 @@ class ExploreContentCell: UITableViewCell {
     }
     
     func setUp() {
-        // Background Image
         backgroundImageView.contentMode = .scaleAspectFill
         backgroundImageView.clipsToBounds = true
         
-        // Top Description Label
         topDescLabel.font = UIFont.systemFont(ofSize: 13)
         topDescLabel.numberOfLines = 0
         
-        // Title Label
         titleLabel.font = UIFont.boldSystemFont(ofSize: 17)
         titleLabel.numberOfLines = 0
         
-        // Promo Message Label
         promoMessageLabel.font = UIFont.systemFont(ofSize: 11)
         promoMessageLabel.numberOfLines = 0
-        promoMessageLabel.textColor = .red // Example color for promo emphasis
         
-        // Bottom Description Label
         bottomDescLabel.font = .systemFont(ofSize: 13)
         bottomDescLabel.numberOfLines = 0
         
-        // Content Stack View
         contentStackView.axis = .vertical
         contentStackView.spacing = 8
         contentStackView.distribution = .fillEqually
         contentStackView.alignment = .leading
     }
     
-//    func configure(with data: ExploreCellData, viewModel: ExploreViewModel) {
-//        titleLabel.text = data.title
-//        
-//        cancellable = viewModel.fetchImage(from: data.backgroundImage)
-//            .sink { [weak self] image in
-//                self?.backgroundImageView.image = image
-//            }
-//    }
+    func configure(with data: ExploreCellData) {
+        topDescLabel.text = data.topDescription
+        titleLabel.text = data.title
+        promoMessageLabel.text = data.promoMessage
+        bottomDescLabel.text = data.bottomDescription
+        
+        if let imageUrl = URL(string: data.backgroundImage) {
+            loadImage(from: imageUrl)
+        }
+        
+        for item in data.content {
+            let button = createContentButton(for: item)
+            contentStackView.addArrangedSubview(button)
+        }
+    }
+    
+    private func createContentButton(for contentItem: ExploreCellData.Content) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(contentItem.title, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.contentHorizontalAlignment = .leading
+        
+        button.addAction(UIAction { _ in
+            if let url = URL(string: contentItem.target) {
+                UIApplication.shared.open(url)
+            }
+        }, for: .touchUpInside)
+        
+        return button
+    }
+    
+    private func loadImage(from url: URL) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self?.backgroundImageView.image = image
+                }
+            }
+        }.resume()
+    }
 }
 
